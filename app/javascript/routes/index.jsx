cc9660bd73e0ca8a0e6152fc471782c142049b5b
Manijeh Shirzadeh <manijeh.shirzadeh63@gmail.com>
2024-01-06 00:50:36 +1100

    added create a new recipe page

--- app/javascript/routes/index.jsx
+++ app/javascript/routes/index.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../components/Home";
 import Recipes from "../components/Recipes";
 import Recipe from "../components/Recipe";
+import NewRecipe from "../components/NewRecipe";
 
 export default (
   <Router>
     <Routes>
-      <Route path="/recipe/:id" element={<Recipe />} />
-      <Route path="/recipes" element={<Recipes />} />
+      <Route path="/recipe/:id" exact element={<Recipe />} />
+      <Route path="/recipe" exact element={<NewRecipe />} />
+      <Route path="/recipes" exact element={<Recipes />} />
       <Route path="/" element={<Home />} />
     </Routes>
   </Router>
